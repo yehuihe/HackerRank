14b4056872a726826cca3d9f7eb13802e010c806
tengma <yehui.he@hotmail.com>
2021-10-26 18:23:43 +0800

    first try-out commit

---
+++ Interview Preparation Kit/Warm-up Challenges/sales_by_match.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    """sockMerchant function
+
+    Parameters
+    ----------
+    n : int
+        The number of socks in the pile.
+    ar : list
+        The colors of each sock.
+
+    Returns
+    -------
+    pairs: int
+        The number of pairs.
+    """
+    pairs = 0
+    sock_colors = set()
+    for i, v in enumerate(ar):
+        if v not in sock_colors:
+            count = 0
+            for j in range(i, n):
+                if ar[j] == v:
+                    count += 1
+            pairs += (count // 2)
+            sock_colors.add(v)
+    return pairs
+    # One liner
+    # return sum([ar.count(i) // 2 for i in set(ar)])
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
